0b8e0e98f23e7cde56375622e4ca0170d704370a
d3adv3gas <jamie.mckay@j2k2.co.uk>
2017-12-10 16:26:02 +0000

    removed date  comment

--- j2k2-test-build.tf
+++ j2k2-test-build.tf
@@ -1,6 +1,4 @@
 # Author Jamie McKay
-# Version 0.1
-# Date 10/12/2017
 
 # Script Goals - Build a 2 Tier Environment (web/app, Data). Load Balanced Web front end (basic HTML which reads data in a DB (simple String)) using ELB. 
 # Web servers deployed across AZs in Ireland. mySQL DB using RDS.
