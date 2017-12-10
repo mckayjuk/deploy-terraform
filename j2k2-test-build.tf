67534515efceac97615359e14e9d311d40df2e24
d3adv3gas <jamie.mckay@j2k2.co.uk>
2017-12-10 16:51:08 +0000

    Clarified subnet is public in comments

--- j2k2-test-build.tf
+++ j2k2-test-build.tf
@@ -17,7 +17,7 @@ provider "aws" {
 resource "aws_instance" "j2k2-test-build" {
   ami           = "ami-785db401" # Machine Version
   instance_type = "t2.micro" # Instance Type
-  subnet_id     = "subnet-faf02fa1" # Add to this subnet
+  subnet_id     = "subnet-faf02fa1" # Add to this Public subnet
   key_name      = "j2k2lablinux" # Use this key
   security_groups = ["sg-1f8df464"] # Add to the Bastion Security Group
   associate_public_ip_address = "true" # Add a Public IP
