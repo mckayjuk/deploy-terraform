d44ba7e60b653e1330a561ecbfe4d05cd8afa18c
Jamie McKay <V3gas@macbook-pro-4.home>
2018-01-11 11:14:50 +0000

    Adding new files for enviornment builds

---
+++ web-env-variables.tf
@@ -0,0 +1,16 @@
+#variable "access_key" {} # Pulled in from file using 'terraform apply -var-file e:\Scripts\Projects\terraform\terraform.tfvars'
+#variable "secret_key" {} # Pulled in from file using 'terraform apply -var-file e:\Scripts\Projects\terraform\terraform.tfvars'
+variable "region" {
+  default = "eu-west-1"
+}
+variable "public-subnets" {
+  description = "Run the EC2 Instances in these Public Subnets"
+  type = "list"
+  default = ["subnet-faf02fa1", "subnet-46178c0f", "subnet-304fdc57"]
+}
+
+variable "web-ports" {
+  description = "The port which will be used to connect to the web servers"
+  type = "string"
+  default = 8080
+}
