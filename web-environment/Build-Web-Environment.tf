580d90f192e5e122f4c5147bd7a01902564ddf16
Jamie McKay <V3gas@macbook-pro-4.home>
2018-01-13 10:45:41 +0000

    Updated Build Config Structure for easier reading.

--- web-environment/Build-Web-Environment.tf
+++ web-environment/Build-Web-Environment.tf
@@ -1,12 +1,13 @@
 ############################################################################################
 # Author Jamie McKay
 
-# Script Goals - Build 2 Load Balanced Web servers which can be accessed via tha bastion over SSH. 
+# Script Goals - Build and Auto scaled, Load Balanced set of web servers. 
 # Web servers deployed across AZs in Ireland
 # This script is stored in GitHub - git@github.com:d3adv3gas/j2k2-deploy
 
-############################################################################################
-# Setup the Provider - Variable provied by file
+# Note, since Terraform is declarative, the order of resource creation is not important (other than for easier comprehension by the reader)
+
+########## Setup the Provider - Variable provied by file ###################
 provider "aws" {
   profile   = "default"
   
@@ -21,7 +22,7 @@ provider "aws" {
   region     = "${var.region}"
 }
 
-# Configure Terraform 
+########## Configure Terraform ########################################
 terraform {
   backend "s3" { # Use the noted S3 bucket to store state
     bucket  = "j2k2-tf-bucket"
@@ -31,20 +32,20 @@ terraform {
   }
 }
 
-# Specify Data Surces
+########## Specify Data Surces ########################################
 data "aws_availability_zones" "all" {} # List all AZs available
 
-# Create an Auto Scaling Group Launch Configuration
+########## Create an Auto Scaling Group Launch Configuration ##########
 resource "aws_launch_configuration" "Web-lc" {
-  name_prefix = "web-lc-"
+  name_prefix = "web-lc-" # don't pin the name as it affects ability to disconnect and recreate.
   image_id = "ami-785db401" # Machine Version
   instance_type = "t2.micro" # Instance Type
   key_name = "j2k2lablinux" # Use this key
   security_groups = ["${aws_security_group.web-sg.id}"] # Add to the Web-sg Security Group
   
-  user_data = <<-EOF
+  user_data = <<-EOF # Configure user data... i.e. config to apply locally (without the use of remote-exec)
     #!/bin/bash
-    echo "Hello - I am server a server created via an AutoScaling Group" > index.html
+    echo "Hello - I am server a server created via an AutoScaling Group" > index.html #creates a simple web page
     nohup busybox httpd -f -p ${var.web-ports} &
     EOF
 
@@ -53,7 +54,7 @@ resource "aws_launch_configuration" "Web-lc" {
   }
 }
 
-# Create a VPC
+########## Create a VPC ################################################
 resource "aws_vpc" "testapp-dev-vpc" {
   cidr_block = "172.16.4.0/22"
 
@@ -62,7 +63,7 @@ resource "aws_vpc" "testapp-dev-vpc" {
   }
 }
 
-# Create a Private subnet in eu-west 1a
+########## Create a Private subnet in eu-west 1a #######################
 resource "aws_subnet" "testapp-dev-euw1a-private" {
   vpc_id     = "${aws_vpc.testapp-dev-vpc.id}"
   availability_zone = "${data.aws_availability_zones.all.names[0]}"
@@ -73,7 +74,7 @@ resource "aws_subnet" "testapp-dev-euw1a-private" {
   }
 }
 
-# Create a Private subnet in eu-west 1b
+########## Create a Private subnet in eu-west 1b #######################
 resource "aws_subnet" "testapp-dev-euw1b-private" {
   vpc_id     = "${aws_vpc.testapp-dev-vpc.id}"
   availability_zone = "${data.aws_availability_zones.all.names[1]}"
@@ -84,7 +85,8 @@ resource "aws_subnet" "testapp-dev-euw1b-private" {
   }
 }
 
-# Create a Private subnet in eu-west 1c
+########## Create a Private subnet in eu-west 1c #######################
+
 resource "aws_subnet" "testapp-dev-euw1c-private" {
   vpc_id     = "${aws_vpc.testapp-dev-vpc.id}"
   availability_zone = "${data.aws_availability_zones.all.names[2]}"
@@ -95,7 +97,8 @@ resource "aws_subnet" "testapp-dev-euw1c-private" {
   }
 }
 
-# Create the web autoscaling group
+########## Create the web autoscaling group#############################
+# 
 resource "aws_autoscaling_group" "web-asg" {
   name_prefix = "web-asg-"
   launch_configuration = "${aws_launch_configuration.Web-lc.id}"
@@ -116,6 +119,9 @@ resource "aws_autoscaling_group" "web-asg" {
   }
 }
 
+########### Create the web server load balancer #############################
+
+########### Create the web Security Group #############################
 resource "aws_security_group" "web-sg" { # Create a security group in Lab VPC
   name_prefix = "web-sg-"
   vpc_id = "${aws_vpc.testapp-dev-vpc.id}"
@@ -124,7 +130,7 @@ resource "aws_security_group" "web-sg" { # Create a security group in Lab VPC
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["151.224.16.41/32","172.16.0.106/32"]
+    cidr_blocks = ["172.16.0.106/32"] # Which IPs does this relate to? You can also use other security groups
   }
 
   lifecycle {
